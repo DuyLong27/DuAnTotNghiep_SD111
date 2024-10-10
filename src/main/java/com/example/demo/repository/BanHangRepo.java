765ba3740d1052f0433246e514dc3c5decf2c4c7
DuyLong27 <longldbph41060@gmail.com>
2024-10-10 17:03:18 +0700

    update folder

--- src/main/java/com/example/demo/repository/BanHangRepo.java
+++ src/main/java/com/example/demo/repository/BanHangRepo.java
@@ -4,6 +4,5 @@ import com.example.demo.entity.SanPham;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface BanHangRepo extends JpaRepository<SanPham, Integer> {
+public interface BanHangRepo {
 }
