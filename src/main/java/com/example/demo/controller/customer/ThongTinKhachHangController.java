421237e0d7812512287614b490d7e03e599ac511
DuyLong27 <longldbph41060@gmail.com>
2024-10-11 22:40:33 +0700

    update

--- src/main/java/com/example/demo/controller/customer/ThongTinKhachHangController.java
+++ src/main/java/com/example/demo/controller/customer/ThongTinKhachHangController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller.customers;
+package com.example.demo.controller.customer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
