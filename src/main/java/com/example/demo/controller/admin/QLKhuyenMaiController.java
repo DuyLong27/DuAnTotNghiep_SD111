e261c8f54b473a63f718b4d1ad10d71b75afe73c
nguyenltph41041 <nguyenltph41041@fpt.edu.vn>
2024-10-12 15:41:45 +0700

    nguyen cap nhat qly khuyen mai

--- src/main/java/com/example/demo/controller/admin/QLKhuyenMaiController.java
+++ src/main/java/com/example/demo/controller/admin/QLKhuyenMaiController.java
@@ -1,37 +1,63 @@
 package com.example.demo.controller.admin;
 
+import com.example.demo.entity.KhuyenMai;
+import com.example.demo.repository.KhuyenMaiRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/khuyen-mai")
 public class QLKhuyenMaiController {
+    @Autowired
+    KhuyenMaiRepo khuyenMaiRepo;
 
     @GetMapping("/hien-thi")
-    public String hienThi(Model model){
-//        model.addAttribute("listKhuyenMai",khuyenMaiRepository.findAll());
+    public String view(@RequestParam(defaultValue = "0") int page,
+                          @RequestParam(defaultValue = "5") int size, Model model) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<KhuyenMai> khuyenMaiPage = khuyenMaiRepo.findAll(pageable);
+
+        model.addAttribute("listKhuyenMai", khuyenMaiPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", khuyenMaiPage.getTotalPages());
         return "admin/ql_khuyen_mai/khuyen-mai";
     }
 
     @GetMapping("/chi-tiet")
-    public String trangDetail(@RequestParam("id")Integer id, Model model){
-//        model.addAttribute("khuyenMaiDetail",khuyenMaiRepository.findById(id).get());
+    public String detailPage(@RequestParam("id")Integer id, Model model){
+        model.addAttribute("khuyenMaiDetail",khuyenMaiRepo.findById(id).get());
         return "admin/ql_khuyen_mai/chi-tiet";
     }
 
     @GetMapping ("/sua/{id}")
-    public String trangSua(@PathVariable("id") Integer id, Model model){
-//        model.addAttribute("khuyenMaiDetail",khuyenMaiRepository.findById(id).get());
+    public String editPage(@PathVariable("id") Integer id, Model model){
+        model.addAttribute("khuyenMaiEdit",khuyenMaiRepo.findById(id).get());
         return "admin/ql_khuyen_mai/sua";
     }
 
-    @GetMapping("/them")
-    public String trangAdd(){
-        return "admin/ql_khuyen_mai/them";
+    @PostMapping("/them")
+    public String add(KhuyenMai khuyenMai){
+        khuyenMaiRepo.save(khuyenMai);
+        return "redirect:/khuyen-mai/hien-thi";
+    }
+
+    @GetMapping("/xoa")
+    public String delete(@RequestParam("id") Integer id){
+        khuyenMaiRepo.deleteById(id);
+        return "redirect:/khuyen-mai/hien-thi";
+    }
+
+    @PostMapping("/sua/{id}")
+    public String edit(@PathVariable("id") Integer id, KhuyenMai khuyenMai){
+        khuyenMai.setIdKhuyenMai(id);
+        khuyenMaiRepo.save(khuyenMai);
+        return "redirect:/khuyen-mai/hien-thi";
     }
 }
+
