eda1145d668a27998def16e1c50739d75d12c008
DuyLong27 <longldbph41060@gmail.com>
2024-10-17 13:39:33 +0700

    update ban hang

--- src/main/java/com/example/demo/entity/HoaDon.java
+++ src/main/java/com/example/demo/entity/HoaDon.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -20,5 +21,18 @@ public class HoaDon {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_hoa_don")
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_khach_hang")
+    private KhachHang id_khach_hang;
+
+    @Column(name = "so_hoa_don")
+    private String so_hoa_don;
+
+    @Column(name = "ngay_tao")
+    private Date ngay_tao;
+
+    @Column(name = "tinh_trang")
+    private Integer tinh_trang;
 
 }
