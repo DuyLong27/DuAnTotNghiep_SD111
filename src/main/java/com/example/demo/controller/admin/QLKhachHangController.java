4ab2ae972f753ce87bb141406c4957444f256818
6thFebruary <anvhph35057@fpt.edu.vn>
2024-10-21 04:08:47 +0700

    QLKH new

---
+++ src/main/java/com/example/demo/controller/admin/QLKhachHangController.java
@@ -0,0 +1,84 @@
+package com.example.demo.controller.admin;
+
+import com.example.demo.entity.KhachHang;
+import com.example.demo.repository.KhachHangRepo;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/khach-hang")
+public class QLKhachHangController {
+    @Autowired
+    KhachHangRepo repo;
+
+    @PostMapping("/add")
+    public String addNV(Model model, @Valid @ModelAttribute("data") KhachHang khachHang,
+                        BindingResult validate, RedirectAttributes redirectAttributes) {
+        if (validate.hasErrors()) {
+            model.addAttribute("data",khachHang);
+            return "admin/ql_khach_hang/QuanLyKhachHang";
+        }
+        repo.save(khachHang);
+        redirectAttributes.addFlashAttribute("message", "Thêm thành công!");
+        return "redirect:/khach-hang/hien-thi";
+    }
+
+    @GetMapping("/hien-thi")
+    public String QuanLyKhachHang(@RequestParam(name = "page", defaultValue = "0") int pageNo,
+                                  @RequestParam(name = "size", defaultValue = "5") int pageSize,
+                                  @RequestParam(name = "tinhTrang", required = false) Integer tinhTrang,
+                                  @RequestParam(name = "tenKhachHang", required = false) String tenKhachHang,
+                                  Model model) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Page<KhachHang> page;
+
+        // Xử lý đầu vào tìm kiếm tên nhà cung cấp
+        if (tenKhachHang != null) {
+            // Loại bỏ dấu cách ở đầu và cuối, và thay thế nhiều dấu cách liên tiếp
+            tenKhachHang = tenKhachHang.trim().replaceAll("\\s+", " ");
+        }
+
+        // Tìm kiếm theo tên nhà cung cấp
+        if (tenKhachHang != null && !tenKhachHang.isEmpty()) {
+            page = repo.findBytenKhachHangContainingIgnoreCase(tenKhachHang, pageable);
+        } else if (tinhTrang != null) {
+            page = repo.findByTinhTrang(tinhTrang, pageable);
+        } else {
+            page = repo.findAll(pageable);
+        }
+
+        model.addAttribute("data", page);
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("tinhTrang", tinhTrang);
+        model.addAttribute("tenKhachHang", tenKhachHang); // Để giữ trạng thái tìm kiếm
+        return "admin/ql_khach_hang/QuanLyKhachHang";
+    }
+
+
+    @GetMapping("edit/{id}")
+    public String editProduct(@PathVariable("id") Integer id, Model model) {
+        KhachHang khachHang = repo.findById(id).orElse(null);
+        model.addAttribute("data", khachHang);
+        return "admin/ql_khach_hang/QuanLyKhachHang";
+    }
+
+    @PostMapping("/update")
+    public String updateProduct(@Valid @ModelAttribute("data") KhachHang khachHang, BindingResult validate, Model model, RedirectAttributes redirectAttributes) {
+        if (validate.hasErrors()) {
+            model.addAttribute("data", khachHang);
+            return "admin/ql_khach_hang/QuanLyKhachHang"; // Hoặc trả về một trang cụ thể
+        }
+        repo.save(khachHang);
+        redirectAttributes.addFlashAttribute("message", "Sửa thành công!");
+        return "redirect:/khach-hang/hien-thi";
+    }
+}
---
+++ src/main/java/com/example/demo/entity/KhachHang.java
@@ -0,0 +1,65 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name = "khach_hang")
+
+public class KhachHang {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_khach_hang")
+    private Integer idKhachHang;
+
+    @Column(name = "ten_khach_hang")
+    private String tenKhachHang;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "mat_khau")
+    private String matKhau;
+
+    @Column(name = "so_dien_thoai")
+    private String soDienThoai;
+
+    @Column(name = "dia_chi")
+    private String diaChi;
+
+    @Column(name = "diem_tich_luy")
+    private Integer diemTichLuy;
+
+    @Column(name = "ngay_dang_ky")
+    private LocalDate ngayDangKy;
+
+    @Column(name = "lich_su_mua_hang")
+    private String lichSuMuaHang;
+
+    @Column(name = "khuyen_mai_da_dung")
+    private String khuyenMaiDaDung;
+
+    @Column(name = "tinh_trang")
+    private Integer tinhTrang;
+
+}
